68becb977842b9f107492860be784a0dd1097ebf
seunbayo <seunbayo83@gmail.com>
2023-09-11 10:12:44 +0100

    made adjustment

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,11 +9,11 @@ export const Layout: FC = ({ children }) => {
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <Heading>
-          SuperteamNg bounty <span className='text-sm font-normal align-top text-slate-700'></span>
+          Thank you for choosing to buy me cofee <span className='text-sm font-normal align-top text-slate-700'></span>
         </Heading>
-        <h4 className="md:w-full text-center text-slate-300 my-2">
-          <h2>Scan the QR code to tip me a few usdc </h2>
-        </h4>
+        <h2 className="md:w-full text-center text-slate-300 my-2">
+          <p>Please Scan the QR code or connect your wallet to proceed</p>
+        </h2>
         {children}
         <div className="text-center">
           <RequestAirdrop />
